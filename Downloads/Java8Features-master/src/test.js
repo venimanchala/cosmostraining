0962318bef5845ece89517290a04fbcf891f5a1e
Veni Manchala <Krishnaveni.Manchala@bbc.co.uk>
2015-11-12 15:02:42 +0000

    cosmos training

---
+++ Downloads/Java8Features-master/src/test.js
@@ -0,0 +1,4 @@
+var message = function(name) {
+    print('Hi' + name  );
+    return "greetings from inside javascript function";
+};
---
+++ Downloads/training/veni-cosmos-training/src/test/java/uk/co/bbc/training/BusyTaskTest.java
@@ -0,0 +1,28 @@
+package uk.co.bbc.training;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BusyTaskTest {
+
+    @Test
+    public void runsForSpecifiedNumberAmountOfTime() {
+
+        BusyTask task = new BusyTask(1.2);
+
+        long startTime = currentTime();
+        task.run();
+        long duration = currentTime() - startTime;
+
+        assertThat(duration, is(greaterThan(1100L)));
+        assertThat(duration, is(lessThan(1300L)));
+    }
+
+    private long currentTime() {
+        return System.currentTimeMillis();
+    }
+}
